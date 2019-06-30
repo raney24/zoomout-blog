3381eb2c89b9e3a91d453e24599b1a35e83979f3
Kyle Raney <raney24fcb@gmail.com>
2019-06-30 01:32:53 -0400

    router changes

--- src/app/layout/Router.js
+++ src/app/layout/Router.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { HashRouter, Switch, Route } from 'react-router-dom'
 import Home from './../Home'
 import Blog from './../Blog'
 import BlogPost from './../blog/BlogPost'
 
 const Router = () => (
-  <Switch>
-    <Route exact path='/' component={Home}/>
-    <Route exact path='/blog' component={Blog}/>
-    <Route exact path='/blog/:blogPost' component={BlogPost}/>
-  </Switch>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+      <Route exact path='/blog' component={Blog}/>
+      <Route exact path='/blog/:blogPost' component={BlogPost}/>
+    </Switch>
 )
 
 export default Router
